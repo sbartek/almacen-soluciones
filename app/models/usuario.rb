77a224c6b13de185c8516ffcd98ed46f3e5bb45b
Bartek <bartekskorulski@gmail.com>
2014-09-26 14:59:58 +0200

    adds index for emails in usuarios

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,7 @@
 class Usuario < ActiveRecord::Base
+
+  before_save { self.email = email.downcase }
+
   validates :nombre, presence: true, length: {maximum: 50, minimum: 4}, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925104045) do
+ActiveRecord::Schema.define(version: 20140926123753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,5 +125,7 @@ ActiveRecord::Schema.define(version: 20140925104045) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
 
 end
