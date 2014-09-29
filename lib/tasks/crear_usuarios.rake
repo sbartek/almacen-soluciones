f0832cf7249cf323c705da136e6e05b074de1403
Bartek <bartekskorulski@gmail.com>
2014-09-29 12:15:04 +0200

    validates length of passwd

--- lib/tasks/crear_usuarios.rake
+++ lib/tasks/crear_usuarios.rake
@@ -2,9 +2,12 @@
 desc "Crear usuarios"
 task :crear_usuarios, [:filename] => :environment do
   usuarios = 
-    [{nombre: "JMYus", email: "jmyus@soluciones-sl.com"},
-     {nombre: "Alex Cisneros", email: "acisneros@soluciones-sl.com"},
-     {nombre: "Jorge Sánchez", email: "jsanchez@soluciones-sl.com"}
+    [{nombre: "JMYus", email: "jmyus@soluciones-sl.com", 
+       password: "000000", password_confirmation: "000000"},
+     {nombre: "Alex Cisneros", email: "acisneros@soluciones-sl.com",
+       password: "000000", password_confirmation: "000000"},
+     {nombre: "Jorge Sánchez", email: "jsanchez@soluciones-sl.com",
+       password: "000000", password_confirmation: "000000"},
     ]
   usuarios.each do |usuario|
     Usuario.create!(usuario)
--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -6,63 +6,57 @@ RSpec.describe Usuario, :type => :model do
     @usuario = FactoryGirl.create(:usuario, 
                                    {nombre: "Jorge", 
                                      email: "jorge@ejemplo.com",
-                                     password: "test1",
-                                     password_confirmation: "test1"
+                                     password: "test01",
+                                     password_confirmation: "test01"
                                    }) 
-    # @usuario = Usuario.new({nombre: "Jorge", 
-    #                                 email: "jorge@ejemplo.com",
-    #                                 password: "test1",
-    #                                 password_confirmation: "test1"
-    #                               }) 
-    # @usuario.save
   }
 
   subject { @usuario }
 
   describe "when password is not present" do
     before do
-      @usuario = Usuario.new(nombre: "Example User", email: "user@example.com",
-                          password: " ", password_confirmation: " ")
+      @usuario.password = " "
+      @usuario.password_confirmation = " "
     end
     it { should_not be_valid }
   end
-
   
   it { should respond_to(:nombre) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
-
-  describe "when nombre is not present" do
-    before { @usuario.nombre = " " }
-    it { should_not be_valid }
-  end
+   
+  describe "nombre" do
+    describe "when nombre is not present" do
+      before { @usuario.nombre = " " }
+      it { should_not be_valid }
+    end
   
-  describe "when email is not present" do
-    before { @usuario.email = " " }
-    it { should_not be_valid }
-  end
-
-  describe "#nombre" do
-    
     describe "when #nombre is too long" do
       before { @usuario.nombre = "a" * 51 }
       it { should_not be_valid }
     end
 
-    describe "when #nombre is too short" do
+    describe "when is too short" do
       before { @usuario.nombre = "a" * 3 }
       it { should_not be_valid }
     end
 
   end
   
-  describe "#email" do
+  describe "email" do
 
-    describe "when email format is invalid" do
+    describe "when is not present" do
+      before { @usuario.email = " " }
+      it { should_not be_valid }
+    end
+
+    describe "when its format is invalid" do
       it "should be invalid" do
         addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -73,7 +67,7 @@ RSpec.describe Usuario, :type => :model do
       end
     end
 
-    describe "when email format is valid" do
+    describe "when its format is valid" do
       it "should be valid" do
         addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
         addresses.each do |valid_address|
@@ -83,15 +77,9 @@ RSpec.describe Usuario, :type => :model do
       end
     end
 
-
-    describe "when email address is already taken" do
+    describe "when is already taken" do
       before do
-        @usuario_with_same_email = FactoryGirl.create(:usuario, 
-                                                      {nombre: "Jorge2", 
-                                                        email: "jorge2@ejemplo.com",
-                                                        password: "test1",
-                                                        password_confirmation: "test1"
-                                                      }) 
+        @usuario_with_same_email = FactoryGirl.create(:usuario) 
         @usuario_with_same_email.email = "jorge@ejemplo.com"
       end
       
@@ -99,6 +87,45 @@ RSpec.describe Usuario, :type => :model do
         expect(@usuario_with_same_email).not_to be_valid
       end
     end
+  end
+  
+  describe "password" do
+    describe "when is not present" do
+      before do
+        @usuario.password = @usuario.password_confirmation = " "
+      end
+      it { should_not be_valid }
+    end
+
+    describe "when doesn't match confirmation" do
+      before { @usuario.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+    end
+    
+    describe "when is too short" do
+      before { @usuario.password = @usuario.password_confirmation = "a" * 5}
+      it { should be_invalid }
+    end
+  end
+
+  describe "#authenticate" do
+
+    describe "return value" do
+      let(:found_user) { Usuario.find_by(email: @usuario.email) }
+      
+      describe "with valid password" do
+        it { should eq found_user.authenticate(@usuario.password) }
+      end
+      
+      describe "with invalid password" do
+        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+        subject { user_for_invalid_password }
+        
+        it { should_not eq @usuario }
+        specify { should eq false }
+      end
+    end
+
   end
   
 end
