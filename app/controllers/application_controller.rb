f026d24274a4850cb39760ed4d05924e11676e08
Bartek <bartekskorulski@gmail.com>
2014-09-30 14:37:40 +0200

    no signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :signin_usuario
+  #before_action :signin_usuario
 
   private
 
